490af9d339baa8402e7db8291987636e2797967b
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-11-13 23:51:01 +0200

    draft02

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,7 +4,7 @@ import { ContactItem, Contacts, DeleteButton } from './ContactList.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectContacts, selectFilter } from 'redux/contacts/selector';
 import { deleteContactThunk, fetchDataThunk } from 'redux/contacts/operations';
-import { Title } from 'components/App.styled';
+// import { Title } from 'components/App.styled';
 
 export const ContactList = () => {
   const contacts = useSelector(selectContacts);
